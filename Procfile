6e17ea0e019d3781b84fa9bd4854034270284258
Mohamed <nemedabbes@gmail.com>
2024-10-21 19:10:08 +0100

    adding a result project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn ResultProject.wsgi --log-file -
---
+++ ResultApp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from .forms import SignUpForm
+
+
+
+def home(request):
+	if request.user.is_authenticated:
+		return render(request, 'home.html' , {})
+	else:
+		return render(request, 'landing.html' , {})
+ 
+
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, ("You Have Been Logged In!  Get MEEPING!"))
+			return redirect('home')
+		else:
+			messages.success(request, ("There was an error logging in. Please Try Again..."))
+			return redirect('login')
+
+	else:
+		return render(request, "login.html", {})
+
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("You Have Been Logged Out. Sorry to Meep You Go..."))
+	return redirect('home')
+
+def register_user(request):
+	form = SignUpForm()
+	if request.method == "POST":
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request,user)
+			messages.success(request, ("You have successfully registered! Welcome!"))
+			return redirect('home')
+	
+	return render(request, "register.html", {'form':form})
